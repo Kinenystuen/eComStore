9407d48d23d5bfac89f7f8dd9e0d7832e6b3980b
Kinenystuen <Kine_on1@hotmail.com>
2024-11-20 12:35:32 +0100

    Pages

---
+++ src/_root/pages/Contact.tsx
@@ -0,0 +1,136 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { FormData } from "../../library/types";
+
+function Contact() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="container mx-auto p-8 max-w-lg bg-white rounded-lg shadow-lg my-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        {/* Full Name */}
+        <label
+          htmlFor="fullname"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Full Name
+        </label>
+        <input
+          {...register("fullname", {
+            required: "Full name is required.",
+            minLength: { value: 3, message: "Minimum length is 3 characters." },
+            maxLength: {
+              value: 50,
+              message: "Maximum length is 50 characters.",
+            },
+          })}
+          placeholder="Your full name"
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="fullname"
+          render={({ message }) => (
+            <p className="text-red-500 text-sm">{message}</p>
+          )}
+        />
+
+        {/* Email */}
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
+        </label>
+        <input
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Simple email regex pattern
+              message: "Please enter a valid email address.",
+            },
+          })}
+          placeholder="Your email"
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="email"
+          render={({ message }) => (
+            <p className="text-red-500 text-sm">{message}</p>
+          )}
+        />
+
+        {/* Subject */}
+        <label
+          htmlFor="subject"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Subject
+        </label>
+        <input
+          {...register("subject", {
+            required: "Subject is required.",
+            minLength: { value: 3, message: "Minimum length is 3 characters." },
+            maxLength: {
+              value: 100,
+              message: "Maximum length is 100 characters.",
+            },
+          })}
+          placeholder="Subject"
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="subject"
+          render={({ message }) => (
+            <p className="text-red-500 text-sm">{message}</p>
+          )}
+        />
+
+        {/* Body Text */}
+        <label
+          htmlFor="bodyText"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Description
+        </label>
+        <textarea
+          {...register("bodyText", {
+            required: "Description is required.",
+            minLength: { value: 3, message: "Minimum length is 3 characters." },
+            maxLength: {
+              value: 1000,
+              message: "Maximum length is 2000 characters.",
+            },
+          })}
+          placeholder="Description"
+          className="w-full mt-1 p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 shadow-sm"
+        />
+        <ErrorMessage
+          errors={errors}
+          name="bodyText"
+          render={({ message }) => (
+            <p className="text-red-500 text-sm">{message}</p>
+          )}
+        />
+
+        {/* Submit */}
+        <input
+          type="submit"
+          className="w-full bg-blue-600 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/_root/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home page</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
