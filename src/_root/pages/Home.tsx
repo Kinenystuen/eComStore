36a932dd1afbb9caeaa19806feb0403b85a4a179
Kinenystuen <Kine_on1@hotmail.com>
2024-12-08 11:11:23 +0100

    Homepage with fetching and filtering data

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { useApi } from "../../hooks/UseApi";
+import { Product } from "../../library/types";
+import SearchBar from "../../components/SearchBar";
+import DisplayData from "../../components/display/DisplayData";
+import H1 from "../../components/shared/Typography/H1";
+import Loader from "../../components/shared/ui/Loader";
+
 const Home = () => {
+  const { data, isLoading, isError } = useApi<Product[]>(
+    "https://v2.api.noroff.dev/online-shop"
+  );
+  const totalItems = data?.length || 0;
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const filteredData = data?.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1>Welcome to the Home page</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="mt-8">
+      <H1 className="text-2xl">Products</H1>
+
+      <SearchBar onSearch={(query) => setSearchQuery(query)} />
+
+      {/* Fetch and show data */}
+      {isLoading && <Loader />}
+      {isError && <div>Error fetching data...</div>}
+      {filteredData && (
+        <DisplayData data={filteredData} totalItems={totalItems} />
+      )}
     </div>
   );
 };
