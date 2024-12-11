915e32e80f4e493b84c3840e3322e719797b3c85
Kinenystuen <Kine_on1@hotmail.com>
2024-12-11 14:46:44 +0100

    Added breadcrumb to cart page

--- src/_root/pages/CartPage.tsx
+++ src/_root/pages/CartPage.tsx
@@ -1,11 +1,17 @@
 import CartPageDisplay from "../../components/CartCartPage";
 import CheckoutForm from "../../components/shared/Checkout";
+import Breadcrumb from "../../components/shared/ui/BreadCrumItem";
 import { useCartContext } from "../../context/CartContext";
 
 const CartPage = () => {
   const cartQuantity = useCartContext();
+  const breadcrumbItems = [
+    { label: "Home", href: "/" },
+    { label: "Cart", href: "/cart", current: true }
+  ];
   return (
     <>
+      <Breadcrumb items={breadcrumbItems} className="ms-4" goBack={true} />
       <CartPageDisplay />
       {cartQuantity.state.items.length > 0 && <CheckoutForm />}
     </>
