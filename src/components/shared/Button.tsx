7e76a974fc4b47516b8870e936cb63c664aaa068
Kinenystuen <Kine_on1@hotmail.com>
2024-12-11 16:22:11 +0100

    Small code setup change to add more buttontype colors

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -9,13 +9,33 @@ function Button({
   buttonType,
   type
 }: ButtonProps) {
+  let buttonClass = "";
+
+  switch (buttonType) {
+    case "green":
+      buttonClass =
+        "bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 text-white";
+      break;
+    case "blue":
+      buttonClass =
+        "bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 text-white";
+      break;
+    case "red":
+      buttonClass =
+        "bg-red-500 hover:bg-red-600 dark:bg-red-500 dark:hover:bg-red-600 text-white";
+      break;
+    default:
+      buttonClass =
+        "bg-gray-500 text-white hover:bg-gray-600 dark:bg-BtnColor dark:hover:bg-BtnColor-400";
+  }
+
   return (
     <button
       onClick={onClick}
       title={title}
       aria-label={ariaLabel}
       type={type}
-      className={`button rounded bg-blue-500 text-white hover:bg-blue-600 dark:bg-BtnColor dark:hover:bg-BtnColor-400 ${buttonType === "green" ? "bg-green-500 hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700 text-white" : ""}  ${className}`}
+      className={`button rounded ${buttonClass} ${className}`}
     >
       {children}
     </button>
