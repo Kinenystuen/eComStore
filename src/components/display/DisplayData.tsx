6cbc680d283a3649b31358be6ea37beef1575cf6
Kinenystuen <Kine_on1@hotmail.com>
2024-12-12 14:00:11 +0100

    Price setup change and

--- src/components/display/DisplayData.tsx
+++ src/components/display/DisplayData.tsx
@@ -54,19 +54,19 @@ const DisplayData: React.FC<DisplayDataProps> = ({ data, totalItems }) => {
               </p>
 
               {/* Prices */}
-              <div className="mt-4 flex flex-wrap items-baseline gap-2">
+              {product.discountedPrice < product.price && (
+                <CalcDiscount
+                  price={product.price}
+                  discountedPrice={product.discountedPrice}
+                  className="text-gray-900 dark:text-whiteFont-600 text-sm mt-4"
+                />
+              )}
+              <div className="mt-1 flex flex-wrap items-baseline gap-2">
                 <div className="flex flex-col gap-2">
                   {product.discountedPrice < product.price && (
                     <span className="text-lg font-bold text-green-500 break-words">
                       ${product.discountedPrice.toFixed(2)}
                     </span>
-                  )}
-                  {product.discountedPrice < product.price && (
-                    <CalcDiscount
-                      price={product.price}
-                      discountedPrice={product.discountedPrice}
-                      className="text-gray-900 dark:text-whiteFont-600 text-sm"
-                    />
                   )}
                 </div>
                 <span
