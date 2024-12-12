ad4ac4fd18ca39e2357a7fe5a656304803d1bf9b
Kinenystuen <Kine_on1@hotmail.com>
2024-12-12 16:36:21 +0100

    Meta tag and icon add to error message

--- src/components/shared/ui/ErrorMessage.tsx
+++ src/components/shared/ui/ErrorMessage.tsx
@@ -4,11 +4,17 @@ import H1 from "../Typography/H1";
 interface ErrorMessageProps {
   message: string;
   children?: React.ReactNode;
+  icon?: React.ReactNode;
 }
 
-const ErrorMessage: React.FC<ErrorMessageProps> = ({ message, children }) => {
+const ErrorMessage: React.FC<ErrorMessageProps> = ({
+  message,
+  children,
+  icon
+}) => {
   return (
     <section className="container w-full mb-10 flex justify-center items-center flex-col min-h-[60vh]">
+      {icon}
       <div className="my-10 text-center">
         <H1 className="uppercase font-bold">{message}</H1>
       </div>
