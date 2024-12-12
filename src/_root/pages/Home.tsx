9505e476edb5193f4f4d86e652589bb37f46a6fb
Kinenystuen <Kine_on1@hotmail.com>
2024-12-12 09:59:43 +0100

    Added scroll to top btn

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -6,6 +6,7 @@ import DisplayData from "../../components/display/DisplayData";
 import H1 from "../../components/shared/Typography/H1";
 import Loader from "../../components/shared/ui/Loader";
 import { apiKey } from "../../library/constants";
+import ScrollToTopBtn from "../../components/shared/ui/ScrollToTopBtn";
 
 const Home = () => {
   const { data, isLoading, isError } = useApi<Product[]>(
@@ -21,15 +22,14 @@ const Home = () => {
   return (
     <div className="container mx-auto px-4 mt-8">
       <H1 className="text-2xl">Products</H1>
-
       <SearchBar onSearch={(query) => setSearchQuery(query)} />
-
       {/* Fetch and show data */}
       {isLoading && <Loader />}
       {isError && <div>Error fetching data...</div>}
       {filteredData && (
         <DisplayData data={filteredData} totalItems={totalItems} />
       )}
+      <ScrollToTopBtn />
     </div>
   );
 };
