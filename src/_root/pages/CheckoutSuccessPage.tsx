66c47e2d7dc660570d37ac2d919e081b1c6d49f7
Kinenystuen <Kine_on1@hotmail.com>
2024-12-12 14:16:42 +0100

    margin fix

--- src/_root/pages/CheckoutSuccessPage.tsx
+++ src/_root/pages/CheckoutSuccessPage.tsx
@@ -18,7 +18,7 @@ const CheckoutSuccessPage: React.FC = () => {
   }, [state.items, setOrder, clearCart]);
 
   return (
-    <div className="container mx-auto text-center flex flex-col items-center justify-center mt-16 gap-2">
+    <div className="container mx-auto text-center flex flex-col items-center justify-center my-16 gap-2">
       <H1 className="m-auto text-3xl w-fit font-bold mb-6 motion-preset-confetti animate-confetti-loop">
         Checkout Successful!
       </H1>
